bd01b6a93e92490a912b32739c0f60360d75e993
joy_Ubuntu <iheyjoy@gmail.com>
2017-04-13 07:57:02 +0800

    change some funtion

--- utils.py
+++ utils.py
@@ -15,8 +15,14 @@ def getTrainDate():
     with open('../data/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
+            if (line[3]==0):
+                count=count+1
+            if (count >3):
+                count=0
             if (line[0] != 'center'):
                 samples.append(line)
+
+
 
     train_samples, validation_samples = train_test_split(samples, test_size=df.TrainTestSplitSize)
 
